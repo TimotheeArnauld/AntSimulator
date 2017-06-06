b45d23b14c5cbcf88c5c2563904250850716e6fd
Yousria <yousria.benchadi@gmail.com>
2017-06-06 14:41:54 +0200

    chercher a manger

--- AntSimulator/AntSimulator/ChercherAManger.cs
+++ AntSimulator/AntSimulator/ChercherAManger.cs
@@ -14,7 +14,22 @@ namespace AntSimulator
             int diffY = personnage.position.coordonnes.y - objet.position.coordonnes.y;
             if(diffX < 0)
             {
+                //droite
+                personnage.position.coordonnes.x++;
+            }else if(diffX > 0)
+            {
+                //gauche
+                personnage.position.coordonnes.x--;
+            }
 
+            if(diffY < 0)
+            {
+                //haut
+                personnage.position.coordonnes.y++;
+            }else if (diffY > 0)
+            {
+                //bas
+                personnage.position.coordonnes.y--;
             }
             
         }
