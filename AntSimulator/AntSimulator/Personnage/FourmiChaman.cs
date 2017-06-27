b263fb958c817ed4350d416535bcc8526259db0c
Yousria <yousria.benchadi@gmail.com>
2017-06-28 01:44:17 +0200

    Fourmi chaman

---
+++ AntSimulator/AntSimulator/Personnage/FourmiChaman.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AntSimulator.Personnage
+{
+    class FourmiChaman : Fourmi
+    {
+        public FourmiChaman() : base()
+        {
+
+        }
+
+        public FourmiChaman(String nom, ZoneAbstraite c, int id, EnvironnementAbstrait env) : base(nom, c, id, env)
+        {
+
+        }
+    }
+}
